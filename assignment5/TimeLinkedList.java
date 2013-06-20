68305f7be67a5c8660393b2b98f5badc02afd56d
Erich Newey <narcodis@gmail.com>
2013-06-20 14:38:16 -0600

    setup timing tests

--- assignment5/TimeLinkedList.java
+++ assignment5/TimeLinkedList.java
@@ -1,32 +1,36 @@
 package assignment5;
 
+import java.util.ArrayList;
+
 public class TimeLinkedList 
 {
 	static long timeStart, timeMid, timeEnd, timeActual;
 	static final int timesToLoop = 100;
-	
-	
+	static MyLinkedList<Integer> list1 = new MyLinkedList<Integer>();
+	static ArrayList<Integer> arrList = new ArrayList<Integer>();
 	
 	public static void main(String[] args)
 	{
 		timeStart = System.nanoTime();
-		while (System.nanoTime() - timeStart < 1000000000) { } //thread stabilizer
-		
-		timeStart = System.nanoTime();
-		
-		for (int i=0; i < timesToLoop; i++)
+		while (System.nanoTime() - timeStart < 2000000000) { } //thread stabilizer
+		for (int j = 0; j < 100; j++)
 		{
-			//perform tests here
+			timeStart = System.nanoTime();
+
+			for (int i=0; i < timesToLoop; i++)
+			{
+				arrList.add(0, j); //perform tests here
+			}
+
+			timeMid = System.nanoTime();
+
+			for (int i=0; i < timesToLoop; i++) { } //empty loop to catch runtime of loop itself
+
+			timeEnd = System.nanoTime();
+
+			timeActual = ((timeMid - timeStart) - (timeEnd - timeMid)) / timesToLoop;
+			System.out.println("Time (ns): " + timeActual);
 		}
-		
-		timeMid = System.nanoTime();
-		
-		for (int i=0; i < timesToLoop; i++) {} //empty loop to catch runtime of loop itself
-		
-		timeEnd = System.nanoTime();
-		
-		timeActual = ((timeMid - timeStart) - (timeEnd - timeMid)) / timesToLoop;
-		System.out.println("Time (ns): " + timeActual);
 	}
 
 }
